d8d616d297c8177ca660aeb4fffe82c521522713
luxkyluke <rock_on77500@hotmail.fr>
2017-02-12 15:49:35 +0100

    setup begin

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+require ('../css/style.scss');
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component{
+	render(){
+		return(
+			<h1> Its Works! </h1>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var webpack = require("webpack");
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            {
+                test:/\.scss$/, 
+                loader: "style-loader!css-loader!sass-loader"
+            }
+        ]
+    }
+};
+
+module.exports = config;
