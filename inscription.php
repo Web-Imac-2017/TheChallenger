1865575f7dd96ca27fdac34d34f93f831651db3a
Quentin <quentin54.louis@laposte.net>
2017-02-13 14:40:14 +0100

    debut du projet

---
+++ inscription.php
@@ -0,0 +1,24 @@
+<?php 
+
+session_start();
+
+class User
+{
+
+private $pseudo;
+private $password;
+private $email;
+private $desc;
+private $key;
+private $isActive;
+private $birthdate;
+private $cptwarnings;
+
+public function register($pseudo, $password, $email){
+	
+}
+
+}
+
+
+?>
