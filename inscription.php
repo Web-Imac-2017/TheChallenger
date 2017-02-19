4b17acf40b028206b57fd5387a3604753e51bf3b
laureis <issalaure@gmail.com>
2017-02-19 21:27:02 +0100

    les posts des utilisateurs

--- inscription.php
+++ inscription.php
@@ -14,10 +14,46 @@ private $isActive;
 private $birthdate;
 private $cptwarnings;
 
+
+public User($pseudo, $pwd, $mail, $desc, $isActive, $birthdate) {
+	
+	$this->pseudo = $pseudo;
+	$this->password = $pwd;
+	$this->mail = $email;
+	$this->desc = $desc;
+	$this->isActive = 0;
+	$this->birthdate = $birthdate;	
+}
 public function register($pseudo, $password, $email){
 	
 }
 
+public function getActive() {
+	
+	
+}
+
+public function signIn($pseudo, $password) {
+	
+}
+
+public function signOut($pseudo, $password) {
+	
+}
+
+public function likePost() {
+	
+
+}
+
+public function changePassword($newPwd, $confirmationPwd) {
+	
+}
+
+public function changeMail($newMail, $confirmationMail) {
+	
+	//try catch error if $newMail != $confirmationMail 
+}
 }
 
 
