d6caef4930b0e3aa4880848a7e641d32e407a0aa
datross <datross@openmailbox.org>
2017-03-04 11:29:43 +0100

    Étape secondaire, le placement des objets du header est en cours, ça utilise la propriété flex, même si je suis pas sûr que ce soit génial...

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -5,7 +5,7 @@ require("../../css/_header.scss");
 export default class SearchBar extends React.Component {
     render() {
         return(
-            <div className="SearchBar">
+            <div className="search-bar">
                 <form className="search-form">
 		     	    <input type="search" name="Your search" className="search-field"/>
                 </form>
