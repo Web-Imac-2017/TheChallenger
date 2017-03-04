25b3da8c12b797bb4f958416c5764f128970eceb
laureis <issalaure@gmail.com>
2017-03-05 00:43:36 +0100

    controller challenge

---
+++ challengeController.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once("challenge.php");
+
+    class challengeController {
+
+		// Informations/affichage d'un challenge
+		
+        public static function infos($id){
+            $challenge = Challenge::challenge_exists($id);
+            echo(json_encode($publication->toArray()));
+        }
+		
+    }
+?>
