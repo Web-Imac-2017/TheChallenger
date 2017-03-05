d3aa75e47e068334732aa93e3a823f454e88d810
Quentin <quentin54.louis@laposte.net>
2017-03-05 14:18:23 +0100

    mise à jour du contrôler et du user, gestion des routes, on rend le tout plus propre

--- index.php
+++ index.php
@@ -1,20 +1,46 @@
-<?php 
-session_start();
+<?php
+	require_once('vendor/autoload.php');
+	require_once('model/routing.php');
+	require_once('model/session.php');
+
+	//CARE
+	require_once("controller/publicationController.php");
+	require_once("controller/userController.php");
+	require_once("controller/conversationController.php");
+	require_once("controller/testController.php");
+
+	//Permet d'include les classes depuis le controller directement sans avoir à cité le controller
+	function __autoload($class_name){
+		require_once 'controller/'.$class_name . '.php';
+	}
+
+    $routing = new ROUTING();
+    $route = $routing->getRoute();
+
+    $root = $routing->getRoot();
+    $parameter = $routing->getParameter();
+
+	var_dump(Session::getSession());
+
+	//Class Manager or route
+
+	if(strpos($route,".php") !== false){
+		include(__DIR__.'/'.$route);
+	} else {
+		try {
+			$explode = explode("@",$route);
+			$c = $explode[0]; // La classe demandé
+			$m = $explode[1]; // La méthode appelée
+			//Appel de la méthode
+			if($parameter != ""){
+				$c::$m($parameter);
+			} else {
+				$c::$m();
+			}
+		} catch(Exception $e){
+			echo($e->getLine()." : ".$e->getMessage());
+		}
+	}
+
 
 ?>
-
-<html>
-<head>
-	<title>test</title>
-	<meta charset="utf-8">
-</head>
-<body>
-	<form method="POST" action="user.php" autocomplete="on">
-		<input name="name" type="text" maxlength="20" placeholder="nom / name" required>
-		<input name="pwd" type="password" maxlength="100" placeholder="mot de passe" required>
-		<input name="pwdconfirm" type="password" maxlength="100" placeholder="confirmer mot de passe" required>
-		<input name="email" type="text" maxlength="150" placeholder="email" required>
-		<input type="submit">
-	</form>
-</body>
-</html>
