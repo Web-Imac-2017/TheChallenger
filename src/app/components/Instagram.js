c1433eb836647c2ffa2bde3c29efa471f0fea8e3
luxkyluke <rock_on77500@hotmail.fr>
2017-03-06 21:56:33 +0100

    merge footer/2

---
+++ src/app/components/Instagram.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Instagram extends React.Component {
+    render() {
+		
+        return(
+                <div className="instagram col-xs-1 "> 
+					<a href="http://www.firefox.com/" >
+					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" height="20px" width="20px"/>
+					</a>
+				
+                </div>
+				
+		
+				
+        );
+    }
+}
