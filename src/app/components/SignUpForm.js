a595f01b0145482811df388d535dd112008f9fce
luxkyluke <rock_on77500@hotmail.fr>
2017-03-06 20:53:16 +0100

    Begin

--- src/app/components/SignUpForm.js
+++ src/app/components/SignUpForm.js
@@ -34,6 +34,7 @@ export default class SignUpForm extends React.Component{
 
 	render(){
 		var formStyle = {display : this.state.formVisible?'block':'none'}
+		var pwdError = this.state.pwdIsWrong ? " error" : ""
 
 		return(
 			<div>
