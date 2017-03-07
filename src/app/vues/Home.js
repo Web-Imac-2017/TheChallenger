4f73c05bad3a014832f7a3693fdac1b42724ffbe
luxkyluke <rock_on77500@hotmail.fr>
2017-03-07 19:31:21 +0100

    test img path

--- src/app/vues/Home.js
+++ src/app/vues/Home.js
@@ -3,11 +3,24 @@ import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 
 export default class Home extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			url:""
+		};
+	}
+
+/*	handleChangeURL(){
+
+		this.setState({url : "http://localhost:8080/Challenger/Data/background.jpg"});
+	}*/
+
 	render(){
 		return(
 			<div id="home">
 	            <Header/>
 				<h1>HOME</h1>
+	            <img src={this.state.url} width="150px" height="200px"/>
 	            <Footer/>
 			</div>
 		);
