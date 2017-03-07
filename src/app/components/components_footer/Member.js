2c87df9f9f44db66cae64535744a467a3c5fbd20
Couavouz <lisa.couavoux@gmail.com>
2017-03-07 10:29:17 +0100

    About us

--- src/app/components/components_footer/Member.js
+++ src/app/components/components_footer/Member.js
@@ -3,10 +3,10 @@ import React from "react";
 export default class Member extends React.Component {
     render() {
         return(
-            <div className="member col-lg-6">
-            	<img src={this.props.image} className="memberImage"/>	
-            	<h1>{this.props.name}</h1>
-            	<p>{this.props.desc }</p>
+            <div className="member col-xs-6">
+            	<img src={this.props.image} className="col-lg-4"/>	
+            	<h2 className="col-lg-8">{this.props.name}</h2>
+            	<p className="col-lg-8">{this.props.desc }</p>
             </div>
         );
     }
