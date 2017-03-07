bc4c30835ddeec19e6a677ef2750a9716aef8bd2
Couavouz <lisa.couavoux@gmail.com>
2017-03-07 18:53:57 +0100

    responsive about us not finished. Review responsive on Footer

--- src/app/components/components_footer/Member.js
+++ src/app/components/components_footer/Member.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Member extends React.Component {
     render() {
         return(
-            <div className="member col-lg-6">
+            <div className="member col-lg-6 col-sm-10">
             	<img src={this.props.image} className="col-lg-4"/>	
             	<h4 className="col-lg-8">{this.props.name}</h4>
             	<p className="col-lg-8">{this.props.desc }</p>
