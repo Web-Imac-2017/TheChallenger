d96af9b80de002193be8721e9a138841c32b0106
luxkyluke <rock_on77500@hotmail.fr>
2017-03-10 22:44:37 +0100

    begin profil style

--- src/app/components/ProfilBox.js
+++ src/app/components/ProfilBox.js
@@ -4,26 +4,21 @@ import FollowBtn from './assets/FollowBtn.js'
 import Vignette from './assets/Vignette.js'
 
 export default class ProfilBox extends React.Component{
-	constructor(props){
-		super(props);
-
-		this.state ={
-			size : 100,
-		};
-		
-	}
 	render(){
 		return(
 			<div id="profil_box">
-				<Vignette image={this.props.photo} size={this.state.size}/>
-				<h2 className="title">{this.props.name}</h2>
-				<FollowBtn/>
-				<div className="profil_box__nbpost"><b>{this.props.nbPost}</b> Posts</div>
-				<div className="profil_box__nbfollow"><b>{this.props.nbFollower}</b> Followers</div>
-				<div className="profil_box__email">{this.props.email}</div>
-				<div className="profil_box__desc">
-					<h4>Description</h4>
-					<p>{this.props.desc}</p>
+				<Vignette 	className ="profil_box__photo"
+							image={this.props.photo}/>
+				<div className="profil_box__text">
+					<h2 className="title">{this.props.name}</h2>
+					<FollowBtn userId={this.props.id}/>
+					<div className="profil_box__nbpost"><b>{this.props.nbPost}</b> Posts</div>
+					<div className="profil_box__nbfollow"><b>{this.props.nbFollower}</b> Followers</div>
+					<div className="profil_box__email">{this.props.email}</div>
+					<div className="profil_box__desc">
+						<h4>Description</h4>
+						<p>{this.props.desc}</p>
+					</div>
 				</div>				
 			</div>	
 		);
