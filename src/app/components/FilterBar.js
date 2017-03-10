b182bc364073920660b6044555aa1edda881b16e
datross <datross@openmailbox.org>
2017-03-10 11:53:01 +0100

    potsfilter quasi fini

---
+++ src/app/components/FilterBar.js
@@ -0,0 +1,45 @@
+import React 	from "react";
+
+/*
+ Quand on créé une FilterBar, on lui donne en props la fonction de callback (callBackParent) pour actualiser, et une liste des filtres à afficher (filters).
+ Un filtre est sous le forme :
+ {
+ key: myKey // identifie le filtre.
+ label: myLabel // texte affiché sur le boutton de filtre
+ }
+ */
+
+export default class FilterBar extends React.Component{
+	  constructor(props){
+		    super(props);
+        this.setState({
+            key: Object.keys(this.props.filters)[0] // on se met sur le premier filtre par défaut
+        });
+        this.buttons = null;
+	  }
+
+    updateFilter(_key) {
+        if(_key == this.state.key) return null;
+        let currentKey = this.props.filters[_key];
+        this.setState({
+            key: _key
+        });
+        this.buttons = Object.entries(this.props.filters).map(function(entry) {
+            return(
+                    <button filterKey={entry[0]} onClick={()=>{console.log("Boutton clicked!");}}>
+                    {entry[1]}
+                </button>
+                  );
+        });
+        this.props.updateParent();
+        return null;
+    }
+
+	  render(){
+		    return(
+                <div className="posts-container">
+                {this.buttons}
+                </div>
+		    );
+	  }
+}
--- src/app/components/PostsContainer.js
+++ src/app/components/PostsContainer.js
@@ -1,21 +1,23 @@
 import React 	from "react";
 import PostMin 	from "../components/PostMin.js";
+import FilterBar from "../components/FilterBar.js";
+import Utility from './../utilities/utility.js';
 
 export default class PostsContainer extends React.Component{
 	  constructor(props){
 		    super(props);
         this.setState({
-            posts: [
-                    <PostMin postId={1} />,
-                    <PostMin postId={2} />
-            ]
+            posts: null
         });
-
+        // Utility.getJSON("api/user/show/"+postId, this);
+        this.filterBar = <FilterBar updateParent={()=>console.log("click")} filters={{"all": "All", "pasAll":"Pas All"}} />;
 	  }
 
 	  render(){
 		    return(
             <div className="posts-container">
+                {this.filterBar}
+            {this.state.posts}
             </div>
 		    );
 	  }
