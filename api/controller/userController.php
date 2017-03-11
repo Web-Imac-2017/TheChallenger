f4eca515cc3637748f20db12ae1c5d7fd8a52bc2
laureis <issalaure@gmail.com>
2017-03-11 12:11:30 +0100

    id connected user

--- api/controller/userController.php
+++ api/controller/userController.php
@@ -171,6 +171,28 @@ class userController{
 		}
 	}
 
+	public static function id_connected(){
+		
+		if (!empty($_COOKIE['id']) && !empty($_COOKIE['name']) && !empty($_COOKIE['pwd']) && !empty($_COOKIE['rank']))
+		{
+			global $db;
+			$query= $db->prepare('SELECT id,name,pwd,rank FROM thechallenger.user WHERE id=:id');
+			$query->bindParam(':id', $_COOKIE['id'], PDO::PARAM_INT);
+			$query->execute();
+			$datas=$query->fetch();
+			$query->CloseCursor();
+
+			if($_COOKIE['id']==$datas['id'] && $_COOKIE['pwd']==$datas['pwd'] && $_COOKIE['name']==$datas['name'] && $_COOKIE['rank']==$datas['rank'])
+			{
+				$item = [
+				
+					"id" => $_COOKIE['id']
+				];
+				echo (json_encode($item));
+			}
+		}
+	}
+	
 	public static function logout(){
 		global $user;
 		//s'il n'est pas connecté
