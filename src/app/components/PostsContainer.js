d5fe51fb4a81d6366f3b515109f5914ce0af5c8a
luxkyluke <rock_on77500@hotmail.fr>
2017-03-11 23:48:14 +0100

    correction some front-back bugs

--- src/app/components/PostsContainer.js
+++ src/app/components/PostsContainer.js
@@ -15,11 +15,11 @@ export default class PostsContainer extends React.Component{
             Utility.query(this.props.query, this.callBackData.bind(this));
             console.log("POSTS REQUETES ENVOYEE");
             // remplissage par défaut
-            this.setState ({
+            this.state = {
                 posts: this.state.postsIds.map(()=>{    
                     return(<PostMin postId={1} />);
                 })
-            });
+            };
         }
         
         this.filterBar = <FilterBar updateParent={()=>console.log("click")} filters={{"all": "All", "pasAll":"Pas All"}} />;
@@ -32,7 +32,7 @@ export default class PostsContainer extends React.Component{
         console.log(data);
         this.setState({
             postsIds: data,
-            posts: data == null ? null : data.map((post) => {return(<PostMin postId={post.id} />);})
+            posts: data == null ? null : data.map((id) => {return(<PostMin postId={id} />);})
         });
         this.updatePostsFiltered("all");
     }
