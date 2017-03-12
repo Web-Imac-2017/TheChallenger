4a124449343d49c068dc037530e3a980c3584908
ValentinBcn <valentin.bacon56@gmail.com>
2017-03-12 15:09:21 +0100

    fonctions header
    suite aux problème de commit, j'ai refait la fonctions de grisement du header lors du survol de la souris sur le search-field

--- src/app/components/header/SearchBar.js
+++ src/app/components/header/SearchBar.js
@@ -1,12 +1,36 @@
 import React from "react";
-
+import ReactDom from "react-dom";
 
 export default class SearchBar extends React.Component {
-    render() {
+    constructor (props){		
+		super(props);
+		
+		
+			
+
+		this.mouseOver = this.mouseOver.bind(this);
+        this.mouseOut = this.mouseOut.bind(this);
+		
+		
+	}
+	
+	mouseOver() {
+       
+		ReactDom.findDOMNode(document.getElementById("logo-area")).style="opacity : 0.2; " ;
+		ReactDom.findDOMNode(document.getElementById("user-menu")).style="opacity : 0.2; " ;
+    }
+	
+    mouseOut() {
+        
+		ReactDom.findDOMNode(document.getElementById("logo-area")).style="opacity : 1; " ;
+		ReactDom.findDOMNode(document.getElementById("user-menu")).style="opacity : 1; " ;
+    }
+	
+	render() {
         return(
             <div className="search-bar">
-                <form className="search-form">
-		     	    <input type="search" name="Your search" className="search-field"/>
+                <form className="search-form" >
+		     	    <input type="search" name="Your search" className="search-field" onMouseOver={this.mouseOver.bind(this)} onMouseOut={this.mouseOut.bind(this)}/>
                 </form>
             </div>
         );
--- src/app/components/header/TabUser.js
+++ src/app/components/header/TabUser.js
@@ -11,7 +11,7 @@ export default class TabUser extends React.Component {
   render() {
     return(
       <div className="tab-user-menu" style={{display:"none"}}>		
-				<button className="access-profil"> My Profil </button>
+				<button className="access-profil">Profil </button>
 				<button className="disconnect" onClick={this.handleDisconnect.bind(this)}> Disconnect </button>
       </div>
     );
