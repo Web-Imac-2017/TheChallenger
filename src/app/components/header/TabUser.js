a6e7db1f9aac108ccbc8e1e083e41ef8ac349dab
luxkyluke <rock_on77500@hotmail.fr>
2017-03-13 23:46:13 +0100

    user logout ok

--- src/app/components/header/TabUser.js
+++ src/app/components/header/TabUser.js
@@ -4,15 +4,12 @@ import Utility from "./../../utilities/utility.js"
 
 export default class TabUser extends React.Component {
 	
-  handleDisconnect(){
-    Utility.query('api/user/logout/');
-  }
 
   render() {
     //<button className="access-profil">Profil </button>
     return(
       <div className="tab-user-menu" style={{display:"none"}}>		
-				<button className="disconnect" onClick={this.handleDisconnect.bind(this)}> Disconnect </button>
+				<a className="disconnect" href='api/user/logout/'> Disconnect </a>
       </div>
     );
   }
