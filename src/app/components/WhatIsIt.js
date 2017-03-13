a69fdd5bfd00521016e02577c20533a63ab8822d
ValentinBcn <valentin.bacon56@gmail.com>
2017-03-13 10:06:12 +0100

    css - Page what is it
    Css page what is it, 
    responsive

--- src/app/components/WhatIsIt.js
+++ src/app/components/WhatIsIt.js
@@ -9,26 +9,13 @@ export default class Login extends React.Component{
 
 	  this.state = {
 	  	url : './../../img/background2.jpg',
-	  	artist: "jeanMi"
+	  	artist: "Cheval253"
 	  };
 
-	  this.getRandomImg();
-	}
 	
-
-	getRandomImg(){
-		Utility.query("api/post/getRandomBackground/", this.callback.bind(this));
-		
-
 	}
 
-	callback(data){
-		if(typeof data === 'undefined' || data.url == null)
-			return;
-		const path = Utility.getPublicPath();
-		this.setState ({url : path+data.url});
-		this.setState ({artist : data.user});
-	}
+	
 
 
 	render(){
@@ -40,20 +27,35 @@ export default class Login extends React.Component{
 		return(
 			<div id="what-is-it">
 				
-				<div className="background_img" style={divStyle}>
-					<div className="title-presentation" > THE CHALLENGER
+				<div className="background_img " style={divStyle}>
+					
+					<div className="img " style={divStyle}>
 					</div>
 					
-					<div className="text-question" > <div className="borderBottom"> <i> What is it ? </i> </div>
+					<div className="overlay ">
+					</div>
+					
+					<div className="title-presentation" > 
+						THE CHALLENGER
+					</div>
+					
+					<div className="text-question" > 
+							<div className="borderBottom"> <i> 
+								What is it ? </i>				
+							</div>
 					</div>
 					
 					<div className = "ligne-vertical">
 					</div>
 					
-					<div className="text-presentation" > It's a place 
-						where you can share any type of content (text, video, pictures, gif, ... ) to challenge anyone and anywhere in the world
+					<div className="text-presentation" > 
+						It's a place where you can share any type of content 
+						(text, video, pictures, gif, ... ) to challenge
+						anyone and anywhere in the world
 					</div>
+					
 				</div>
+				
 				<span className="artist"><p>by {this.state.artist}</p></span>
 			
 			</div>
