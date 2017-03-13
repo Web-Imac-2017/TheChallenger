026e465d7a6b9b46eef625e36bf1d083b02457ec
Couavouz <lisa.couavoux@gmail.com>
2017-03-13 23:54:54 +0100

    umpload in  challenge vue

--- src/app/vues/Home.js
+++ src/app/vues/Home.js
@@ -3,14 +3,10 @@ import Header 	from "../components/Header.js";
 import Footer 	from "../components/Footer.js";
 import PostsContainer from "../components/PostsContainer.js";
 import ChallengeContainer from "../components/ChallengeContainer.js";
-import UploadPost from "./../components/UploadPost.js";
 
 export default class Home extends React.Component{
 	constructor(props){
 		super(props);
-	}
-	handleUploadBtnClick(){
-		this.refs.upload.open();
 	}
 	
 	render(){
@@ -21,10 +17,6 @@ export default class Home extends React.Component{
 			        <h1>Pick up your Challenge</h1>
 			        <ChallengeContainer/>
 		        </div>
-		        
-		        <button id="btn" onClick={this.handleUploadBtnClick.bind(this)}>>UPLOAD YOUR CONTENT</button>
-				<UploadPost ref="upload"/>
-				
 	            <h1>Last Winners</h1>
 	            <PostsContainer query={"api/winners/"}/>
 		        <Footer/>
