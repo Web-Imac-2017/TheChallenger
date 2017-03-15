a6c74343fc2f6875ffe7eec52119de34e089d528
Couavouz <lisa.couavoux@gmail.com>
2017-03-15 20:31:54 +0100

    Challenge box + header + aboutus

--- src/app/components/header/TabUser.js
+++ src/app/components/header/TabUser.js
@@ -6,7 +6,6 @@ export default class TabUser extends React.Component {
 	
 
   render() {
-    //<button className="access-profil">Profil </button>
     return(
       <div className="tab-user-menu" style={{display:"none"}}>		
 				<a className="disconnect" href='api/user/logout/'> Disconnect </a>
